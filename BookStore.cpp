3119772a2cddb49218c6924512ffd173079deb9e
EWP80 <erikpw009@gmail.com>
2023-03-14 21:21:58 -0700

    debug

--- BookStore.cpp
+++ BookStore.cpp
@@ -7,222 +7,189 @@
 //
 #include "BookStore.hpp"
 
+
 //
 #include <iostream>
 #include <sstream>
 #include <string>
+
 
 //
 using std::cout, std::cin, std::endl;
 using std::string, std::to_string;
 using std::stringstream;
 
+
 //
 namespace CPSC131::BookStore
 {
 
-    /// Your welcome
-    BookStore::BookStore() {}
-
-    /**
-     * Adjust the store's account balance
-     * Should accept positive or negative adjustments
-     */
-    void BookStore::adjustAccountBalance(int adjustment)
-    {
-        accountBalance += adjustment;
-    }
-
-    /**
-     * Return the store's current account balance
-     */
-    int BookStore::getAccountBalance()
-    {
-        return accountBalance;
-    }
-
-    /**
-     * Find a book by its ISBN
-     *
-     * Return this->bookList.end() if the book isn't found.
-     *
-     * Return an interator pointing to the Book if it is found.
-     */
-    DoublyLinkedList::DoublyLinkedList<Book>::Iterator BookStore::findBook(std::string isbn)
-    {
-        for (auto it = bookList.begin(); it != bookList.end(); ++it)
-        {
-            if (it->isbn() == isbn)
-            {
-                return it;
-            }
+	/// Your welcome
+	BookStore::BookStore() {}
+	
+	/**
+	 * Adjust the store's account balance
+	 * Should accept positive or negative adjustments
+	 */
+	void BookStore::adjustAccountBalance(int adjustment)
+	{
+		account_balance_ += adjustment;
+		
+	}
+	
+	/**
+	 * Return the store's current account balance
+	 */
+	int BookStore::getAccountBalance() {
+		return account_balance_;
+	}
+	
+	/**
+	 * Find a book by its ISBN
+	 * 
+	 * Return this->bookList.end() if the book isn't found.
+	 * 
+	 * Return an interator pointing to the Book if it is found.
+	 */
+	DoublyLinkedList::DoublyLinkedList<Book>::Iterator BookStore::findBook(std::string isbn)
+	{
+		for (auto it = this->bookList.begin(); it != this->bookList.end(); ++it) {
+			if ((*it).getIsbn() == isbn) {
+				return it;
+			}
+		}
+		return this->bookList.end();
+	}
+	
+	
+	/**
+	 * Check whether a book exists, by its ISBN
+	 * 
+	 * Return true if it exists, or false otherwise
+	 */
+	bool BookStore::bookExists(std::string isbn) {
+		return findBook(isbn)!= bookList.end();
+	}
+	
+	/**
+	 * Check the quantity of stock we have for a particular book, by ISBN
+	 * 
+	 * If the book doesn't exist, just return 0
+	 */
+	size_t BookStore::getBookStockAvailable(std::string isbn) {
+		auto it = findBook(isbn);
+		if (it == bookList.end()) {
+			return 0;
+		}
+		return (*it).getStockAvailable();
+	}
+	
+	/**
+	 * Locate a book by ISBN and return a reference to the Book
+	 * 
+	 * If the book doesn't exist, throw an exception
+	 */
+	Book& BookStore::getBook(std::string isbn) {
+		auto it = findBook(isbn);
+        if (it == bookList.end()) {
+            throw std::runtime_error("Book not found");
         }
-
-        return bookList.end();
+        return *it;
+	}
+	
+	/**
+	 * Take a Book instance and add it to inventory
+	 * 
+	 * If the book's ISBN already exists in our store,
+	 * 	simply adjust account balance by the book's price and quantity,
+	 * 	but ignore other details like title and author.
+	 * 
+	 * If the book's ISBN doesn't already exist in our store,
+	 * 	adjust our account balance and push the book into our list
+	 */
+	 void BookStore::purchaseInventory(const Book& book) {
+        auto it = findBook(book.getIsbn());
+        if (it == bookList.end()) {
+            bookList.push_back(book);
+            adjustAccountBalance(book.getPriceCents() * book.getStockAvailable());
+        } else {
+            (*it).adjustStockAvailable(book.getStockAvailable());
+            adjustAccountBalance(book.getPriceCents() * book.getStockAvailable());
+        }
+    }
+	
+	/**
+	 * Take some book details and add the book to our inventory.
+	 * 
+	 * Use the same rules as the other overload for this function.
+	 * 
+	 * You might want to avoid repeating code by simply building a Book
+	 * 	object from the details, then calling the other overload
+	 * 	with the new Book object.
+	 */
+	void BookStore::purchaseInventory(
+        std::string title, std::string author, std::string isbn,
+        size_t price_cents, size_t unit_count) {
+        Book book(title, author, isbn, price_cents, unit_count);
+        purchaseInventory(book);
     }
 
-    /**
-     * Check whether a book exists, by its ISBN
-     *
-     * Return true if it exists, or false otherwise
-     */
-    bool BookStore::bookExists(std::string isbn)
-    {
-        return findBook(isbn) != bookList.end();
+	
+	/**
+	 * Print out inventory.
+	 * Should be in a particular way to earn unit test points.
+	 * Example (ignore the asterisks at the left of this comment block):
+	 * 
+	 * *** Book Store Inventory ***
+	 * "Book1", by Author1 [123] (5 in stock)
+	 * "Book2", by Author2 [456] (19 in stock)
+	 * 
+	 * Should print a trailing std::endl after the last book line, for the unit tests
+	 */
+	void BookStore::printInventory() {
+        std::cout << "*** Book Store Inventory ***" << std::endl;
+        for (auto it = bookList.begin(); it != bookList.end(); ++it) {
+            std::cout << "\"" << (*it).getTitle() << "\", by " << (*it).getAuthor() << " ["
+                      << (*it).getIsbn() << "] (" << (*it).getStockAvailable() << " in stock)" << std::endl;
+        }
+        std::cout << std::endl;
     }
-
-}
-
-/**
- * Check the quantity of stock we have for a particular book, by ISBN
- *
- * If the book doesn't exist, just return 0
- */
-/**
- * Check the quantity of stock we have for a particular book, by ISBN
- *
- * If the book doesn't exist, just return 0
- */
-size_t BookStore::getBookStockAvailable(std::string isbn) const
-{
-    auto it = bookStockAvailable.find(isbn);
-    if (it == bookStockAvailable.end())
-    {
-        return 0;
-    }
-    else
-    {
-        return it->second;
-    }
-}
-
-/**
- * Locate a book by ISBN and return a reference to the Book
- *
- * If the book doesn't exist, throw an exception
- */
-Book &BookStore::getBook(std::string isbn)
-{
-    if (!bookExists(isbn))
-    {
+	/**
+	 * Sell a book to a customer!
+	 * 
+	 * Takes the ISBN of the book, the selling price of the book, and the quantity of books sold
+	 * 
+	 * Uses the same rules as the other overload.
+	 * 
+	 * You may wish to just grab a reference to the book and call the other overload,
+	 * 	to avoid repeating code
+	 */
+	void BookStore::sellToCustomer(std::string isbn, size_t price_cents, size_t quantity) {
+    auto it = findBook(isbn);
+    if (it == bookList.end()) {
         throw std::runtime_error("Book not found");
     }
-
-    // Find the book by its ISBN using the findBook function
-    DoublyLinkedList::DoublyLinkedList<Book>::Iterator it = findBook(isbn);
-
-    return *it;
+    Book& book = *it;
+    sellToCustomer(book, price_cents, quantity);
 }
 
-/**
- * Take a Book instance and add it to inventory
- *
- * If the book's ISBN already exists in our store,
- * 	simply adjust account balance by the book's price and quantity,
- * 	but ignore other details like title and author.
- *
- * If the book's ISBN doesn't already exist in our store,
- * 	adjust our account balance and push the book into our list
- */
-void BookStore::purchaseInventory(const Book &book)
-{
-    if (bookExists(book.isbn()))
-    {
-        adjustAccountBalance(book.price()) * book.quantity();
-        bookStockAvailable[book.isbn()] += book.quantity();
+	/**
+	 * Sell a book to a customer!
+	 * 
+	 * Takes a Book reference, the selling price of the book, and the quantity of books sold
+	 * 
+	 * If we don't have enough of this book in stock for the quantity the customer wants to purchase,
+	 * 	throw an std::range_error
+	 * 
+	 * Otherwise, adjust the stock available in our store, and update our account balance.
+	 */
+	// Method to sell a book to a customer using ISBN
+	void BookStore::sellToCustomer(Book& book, size_t price_cents, size_t quantity) {
+    if (book.getStockAvailable() < quantity) {
+        throw std::range_error("Not enough stock");
     }
-    else
-    {
-        bookList[book.isbn()] = book;
-        bookStockAvailable[book.isbn()] = book.quantity();
-        adjustAccountBalance(book.price() * book.quantity());
-    }
-}
+    book.adjustStockAvailable(-(int)quantity); // Decrease stock available
+    adjustAccountBalance(price_cents * quantity); // Increase account balance
+}	
 
-/**
- * Take some book details and add the book to our inventory.
- *
- * Use the same rules as the other overload for this function.
- *
- * You might want to avoid repeating code by simply building a Book
- * 	object from the details, then calling the other overload
- * 	with the new Book object.
- */
-void BookStore::purchaseInventory(
-    std::string title, std::string author, std::string isbn,
-    size_t price_cents,
-    size_t unit_count)
-{
-    Book book{title, author, isbn, price_cents, unit_count};
-    purchaseInventory(book);
-}
-
-/**
- * Print out inventory.
- * Should be in a particular way to earn unit test points.
- * Example (ignore the asterisks at the left of this comment block):
- *
- * *** Book Store Inventory ***
- * "Book1", by Author1 [123] (5 in stock)
- * "Book2", by Author2 [456] (19 in stock)
- *
- * Should print a trailing std::endl after the last book line, for the unit tests
- */
-void BookStore::printInventory()
-{
-    std::cout << "*** Book Store Inventory ***\n";
-    for (const auto &book : bookList)
-    {
-        std::cout << "\"" << book.title() << "\", by " << book.author() << " ["
-                  << book.isbn() << "] (" << book.stockCount() << " in stock)\n";
-    }
-    std::cout << std::endl;
-}
-
-/**
- * Sell a book to a customer!
- *
- * Takes the ISBN of the book, the selling price of the book, and the quantity of books sold
- *
- * Uses the same rules as the other overload.
- *
- * You may wish to just grab a reference to the book and call the other overload,
- * 	to avoid repeating code
- */
-void BookStore::sellToCustomer(std::string isbn, size_t price_cents, size_t quantity)
-{
-    if (quantity <= 0)
-    {
-        throw std::runtime_error("Invalid quantity");
-    }
-    for (auto &book : bookList)
-    {
-        if (book.isbn() == isbn && book.stockCount() >= quantity)
-        {
-            book.adjustStock(-quantity);
-            adjustAccountBalance(price_cents * quantity);
-            return;
-        }
-    }
-    throw std::runtime_error("Book not found or insufficient stock");
-}
-
-/**
- * Sell a book to a customer!
- *
- * Takes a Book reference, the selling price of the book, and the quantity of books sold
- *
- * If we don't have enough of this book in stock for the quantity the customer wants to purchase,
- * 	throw an std::range_error
- *
- * Otherwise, adjust the stock available in our store, and update our account balance.
- */
-void BookStore::sellToCustomer(Book &book, size_t price_cents, size_t quantity)
-{
-    if (quantity > book.stockCount())
-    {
-        throw std::range_error("Insufficient stock");
-    }
-    book.adjustStock(-quantity);
-    adjustAccountBalance(price_cents * quantity);
 }
